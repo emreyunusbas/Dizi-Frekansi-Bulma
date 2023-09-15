59291af7e5f8a04092aee3bc41d86348cfc82d34
Yunus Emre Baş <yunusemre.bas@ogr.iu.edu.tr>
2023-09-15 07:42:33 +0300

    .
    Update

--- Main.java
+++ Main.java
@@ -1,2 +1,23 @@
 public class Main {
+    public static void main(String[] args) {
+        int[] dizi = {10, 20, 20, 10, 10, 20, 5, 20};
+
+        for (int i = 0; i < dizi.length; i++) {
+            int eleman = dizi[i];
+            int frekans = 0; // Her elemanın başlangıç frekansı 0'dır
+
+            if (eleman == -1) {
+                continue; // Bu eleman daha önce işlendi, atlamak için
+            }
+
+            for (int j = i; j < dizi.length; j++) {
+                if (eleman == dizi[j]) {
+                    frekans++;
+                    dizi[j] = -1; // Elemanı işlendi olarak işaretle
+                }
+            }
+
+            System.out.println(eleman + " sayısı " + frekans + " kere tekrar edildi.");
+        }
+    }
 }
